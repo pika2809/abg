5667ebdba200c1b08c5efcf5a3121f1818191152
Priyanka Jadhav <priyankajadhav023@gmail.com>
2021-04-24 01:55:32 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    platform = new Ground(150,305,300,170);
     
     ground = new Ground(600,height,1200,20)
 
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
